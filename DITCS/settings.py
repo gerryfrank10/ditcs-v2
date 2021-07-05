cbd5d92913502572a82104a95dab587c30ce7c17
medusa <gerryfrank720@gmail.com>
2021-07-05 23:35:16 +0300

    added another dbms

--- DITCS/settings.py
+++ DITCS/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'DITCS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db2.sqlite3',
     }
 }
 

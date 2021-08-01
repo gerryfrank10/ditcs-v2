cab3f8c113506fd269188552f8a974d0508cfb9a
medusa <gerryfrank720@gmail.com>
2021-08-01 16:22:39 +0300

    django-pytest

--- DITCS/settings.py
+++ DITCS/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import django_heroku
 import os
+import django
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

8ccba6b7ec2e00535a0b925e79b3766bee607efa
medusa <gerryfrank720@gmail.com>
2021-08-01 16:45:06 +0300

    Added form tests and modified travis django db

--- DITCS/settings.py
+++ DITCS/settings.py
@@ -3,7 +3,6 @@ import django_heroku
 import os
 import django
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- traffic/tests.py
+++ traffic/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from traffic.forms import LightsForm
 
 # Create your tests here.
 class TestPage(TestCase):
@@ -6,3 +7,11 @@ class TestPage(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         
+class TestForm(TestCase):
+    def test_validate_light_form(self):
+        form = LightsForm(data={"state": "A"})
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_light_form(self):
+        form = LightsForm(data={"state": "off"})
+        self.assertFalse(form.is_valid())
